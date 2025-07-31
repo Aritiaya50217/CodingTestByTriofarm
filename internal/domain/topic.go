3952ed0d675ef11b2e75762fe9b13596094b616e
artitaya <artitaya2466@gmail.com>
2025-07-31 15:21:51 +0700

    initial project

---
+++ internal/domain/topic.go
@@ -0,0 +1,17 @@
+package domain
+
+import "time"
+
+const (
+	Medicine       = "ยา"
+	Vitamins       = "วิตามิน"
+	Microorganisms = "จุลินทรีย์"
+	Brands         = "ยี่ห้อ"
+)
+
+type Topic struct {
+	ID        int       `gorm:"primaryKey" json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+}
